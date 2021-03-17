6cb8ab2e8150ae21c69951272d6609cd331fe1e8
QUANG MINH PHAM <quangminh1575@gmail.com>
2021-03-17 14:49:08 +0100

    updated

--- resources/views/users/shopping_card.blade.php
+++ resources/views/users/shopping_card.blade.php
@@ -4,39 +4,45 @@
 
 @section('contents')
 
-    @unless(empty($pizza))
-        <table>
-            <tr>
-                <th>NOM</th>
-                <th>PRIX/PRODUIT</th>
-                <th>QUANTITÉ</th>
-                <th>PRIX_TOTAL</th>
-                <th>OPÉRATION</th>
-            </tr>
-            @foreach ($pizza as $p)
+    @forelse ($pizza as $p)
+        @if ($loop->first)
+            <table>
                 <tr>
-                    <td>{{ $p->nom }}</td>
-                    <td>{{ $p->prix }}</td>
-                    <td>
-                        <form action="{{ route('cart.update', ['nom' => $p->nom, 'prix' => $p->prix]) }}">
-                            <input name="quantity" type="number" value="{{ Session::get($p->nom)['pizza_qty'] }}">
-                            <input type="submit" value="save">
-                        </form>
-                    </td>
-                    <td>{{ Session::get($p->nom)['prix_total'] }}</td>
-                    <td><a href="{{ route('delete_from_card', ['nom' => $p->nom, 'id' => $p->id]) }}">Supprimer</a></td>
+                    <th>NOM</th>
+                    <th>PRIX/PRODUIT</th>
+                    <th>QUANTITÉ</th>
+                    <th>PRIX_TOTAL</th>
+                    <th>OPÉRATION</th>
                 </tr>
-            @endforeach
-        </table>
-        <h3>
-            Le Prix Total : {{ Session::get('prixTOTAL') }} $
-        </h3>
-
-        @if (Session::has('ListId'))
-            <h3>
-                <a href="{{ route('confirm_order') }}">Acheter </a>
-            </h3>
         @endif
-    @endunless
+        <tr>
+            <td>{{ $p->nom }}</td>
+            <td>{{ $p->prix }}</td>
+            <td>
+                <form action="{{ route('cart.update', ['nom' => $p->nom, 'prix' => $p->prix]) }}">
+                    <input name="quantity" type="number" value="{{ Session::get($p->nom)['pizza_qty'] }}">
+                    <input type="submit" value="save">
+                </form>
+            </td>
+            <td>{{ Session::get($p->nom)['prix_total'] }}</td>
+            <td><a href="{{ route('delete_from_card', ['nom' => $p->nom, 'id' => $p->id]) }}">Supprimer</a></td>
+        </tr>
+        @if ($loop->last)
+            </table>
+        @endif
+    @empty
+        <p>vous n'avez rien dans le panier </p>
+    @endforelse
+
+    <h3>
+        Le Prix Total : {{ Session::get('prixTOTAL') }} $
+    </h3>
+
+    @if (Session::has('ListId'))
+        <h3>
+            <a href="{{ route('confirm_order') }}">Acheter </a>
+        </h3>
+    @endif
     <a href="{{ route('back_list') }}">Back to Menu</a>
+    
 @endsection
