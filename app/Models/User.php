e79adfabe36b9b47a317959969b2e19f259797f1
QUANG MINH PHAM <quangminh1575@gmail.com>
2021-03-24 13:17:40 +0100

    updated

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,34 +41,11 @@ class User extends Authenticatable
         return $this->type == 'cook';
     }
 
+    public function commandes(){
+        return $this->hasMany(Commande::class);
+    }
 
-    // /**
-    //  * The attributes that are mass assignable.
-    //  *
-    //  * @var array
-    //  */
-    // protected $fillable = [
-    //     'name',
-    //     'email',
-    //     'password',
-    // ];
-
-    // /**
-    //  * The attributes that should be hidden for arrays.
-    //  *
-    //  * @var array
-    //  */
-    // protected $hidden = [
-    //     'password',
-    //     'remember_token',
-    // ];
-
-    // /**
-    //  * The attributes that should be cast to native types.
-    //  *
-    //  * @var array
-    //  */
-    // protected $casts = [
-    //     'email_verified_at' => 'datetime',
-    // ];
+    public function pizzas(){
+        return $this->hasMany(Pizza::class);
+    }
 }
